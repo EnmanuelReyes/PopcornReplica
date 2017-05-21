b8d6dea5038362f496536ead5cacd6eee076ded1
Enmanuel Reyes <EnmanuelReyes@live.com>
2017-05-21 13:10:52 -0400

    Create AzureService
    Create AzureService with dummy values and asynchronous Getters to simulate a webservice call.

---
+++ PopcornReplica/PopcornReplica/Services/AzureService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PopcornReplica.Models;
+
+namespace PopcornReplica.Services
+{
+	public class AzureService
+	{
+
+		public List<Category> Categories { get; set; }
+		public AzureService()
+		{
+			Categories = new List<Category> {
+				new Category() { Name = "Action"},
+				new Category() { Name = "Adventure"},
+				new Category() { Name = "Animation"},
+				new Category() { Name = "Comedy"},
+				new Category() { Name = "Crime"},
+				new Category() { Name = "Documentary"},
+				new Category() { Name = "Drama"},
+				new Category() { Name = "Family"},
+				new Category() { Name = "Fantasy"},
+				new Category() { Name = "History"},
+				new Category() { Name = "Horror"},
+				new Category() { Name = "Music"},
+				new Category() { Name = "Mystery"},
+				new Category() { Name = "Romance"},
+				new Category() { Name = "Science Fiction"},
+				new Category() { Name = "Thriller"},
+				new Category() { Name = "War"},
+				new Category() { Name = "Western"}
+			};
+
+		}
+
+		public async Task<List<Category>> GetCategories()
+		{
+			return Categories;
+		}
+
+
+	}
+}
