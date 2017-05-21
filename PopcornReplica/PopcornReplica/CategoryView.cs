f22ada8057db0f5fb77f0a53044c3f0f00582c30
Enmanuel Reyes <EnmanuelReyes@live.com>
2017-05-21 13:08:35 -0400

    Create CategoryView
    Create CategoryView for displaying a image and a label above the image showing the name of the category.

---
+++ PopcornReplica/PopcornReplica/CategoryView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using PopcornReplica.Models;
+using Xamarin.Forms;
+
+namespace PopcornReplica
+{
+	public class CategoryView : ContentView
+	{
+		public Category Category { get; set; }
+
+		public CategoryView()
+		{
+			Content = new Label { Text = "Hello ContentView" };
+
+		}
+		public CategoryView(Category category)
+		{
+			this.Category = category;
+			var myLabel = new Label()
+			{
+				Text = category.Name,
+				Font = Font.SystemFontOfSize(20, FontAttributes.Bold),
+				TextColor = Color.Black,
+				HorizontalTextAlignment = TextAlignment.Center,
+				VerticalTextAlignment = TextAlignment.End,
+			};
+			var embeddedImage = new Image { Aspect = Aspect.AspectFit };
+			embeddedImage.Source = ImageSource.FromResource("PopcornReplica.beach.jpg");
+
+
+			var grid = new Grid();
+			grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+			grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+			grid.Children.Add(embeddedImage, 0, 0);
+			grid.Children.Add(myLabel, 0, 0);
+			Content = grid;
+		}
+	}
+}
+
---
+++ PopcornReplica/PopcornReplica/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PopcornReplica.Models
+{
+	public class Category
+	{
+		public string Name { get; set; }
+
+		public Category()
+		{
+		}
+	}
+}
