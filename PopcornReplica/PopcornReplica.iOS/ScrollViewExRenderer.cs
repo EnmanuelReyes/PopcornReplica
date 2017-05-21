2c6043d4aa01d8df50a1641a5a322102a25d43da
Enmanuel Reyes <EnmanuelReyes@live.com>
2017-05-20 20:54:13 -0400

    Hide horizontal scrollbar on iOS
    Create a Renderer of ScrollView to disable the horizontal scrollbar

---
+++ PopcornReplica/PopcornReplica.iOS/ScrollViewExRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using PopcornReplica.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ScrollView), typeof(ScrollViewExRenderer))]
+namespace PopcornReplica.iOS
+{
+	public class ScrollViewExRenderer : ScrollViewRenderer
+	{
+		protected override void OnElementChanged(VisualElementChangedEventArgs e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.OldElement != null || this.Element == null)
+				return;
+
+			if (e.OldElement != null)
+				e.OldElement.PropertyChanged -= OnElementPropertyChanged;
+
+			e.NewElement.PropertyChanged += OnElementPropertyChanged;
+
+		}
+
+		protected void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			ShowsHorizontalScrollIndicator = false;
+		}
+	}
+}
