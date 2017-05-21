b5e27bb6d342fcb5fccb14fe65b0d35183464a1e
Enmanuel Reyes <EnmanuelReyes@live.com>
2017-05-21 13:15:08 -0400

    Create WatchlistPage
    Create WatchlistPage for displaying all the categories in a horizontal way to display movies that are in watchlist.

--- PopcornReplica/PopcornReplica/Views/WatchlistPage.xaml.cs
+++ PopcornReplica/PopcornReplica/Views/WatchlistPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using PopcornReplica.Services;
 using Xamarin.Forms;
 
 namespace PopcornReplica.Views
@@ -10,6 +10,47 @@ namespace PopcornReplica.Views
 		public WatchlistPage()
 		{
 			InitializeComponent();
+			LoadCategories();
 		}
+
+		public async void LoadCategories()
+		{
+			AzureService az = new AzureService();
+			foreach (var x in await az.GetCategories())
+			{
+				Label label = new Label
+				{
+					Margin = new Thickness { Right = 10 },
+					Text = x.Name,
+					BindingContext = x,
+					TextColor = Color.Black
+					//TextColor = Color.White,
+				};
+				label.GestureRecognizers.Add(new TapGestureRecognizer((view) => OnLabelClicked(view)));
+
+				this.View.Children.Add(label);
+
+			}
+		}
+
+		void OnLabelClicked(View view)
+		{
+			System.Diagnostics.Debug.WriteLine("clickeo label");
+			AllWhite();
+			((Label)view).TextColor = (Color)Application.Current.Resources["Pink"];
+
+		}
+
+		void AllWhite()
+		{
+			foreach (var x in View.Children)
+			{
+
+				//((Label)x).TextColor = Color.White;
+				((Label)x).TextColor = Color.Black;
+
+			}
+		}
+
 	}
 }
