0d372a0d77eb2f526d753662e7513ef93bf80138
Enmanuel Reyes <EnmanuelReyes@live.com>
2017-05-21 21:10:41 -0400

    Create TabbedPage for navigation

---
+++ PopcornReplica/PopcornReplica/MasterPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PopcornReplica
+{
+	public partial class MasterPage : TabbedPage
+	{
+		public MasterPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
